53f5182543645599cb959b49513d9e326c1e1f4b
tpstar <hanlee12@gmail.com>
2016-09-23 14:43:17 -0400

    trouble install gem pg

--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,10 @@
 class CreateRides < ActiveRecord::Migration
   # Write your migrations here
+  def change
+    create_table :rides do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :attraction, index: true, foreign_key: true
+
+    end
+  end
 end

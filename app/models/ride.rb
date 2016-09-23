d3c78f4d35750706c04b2cea50ee5bc5a8003712
Han Lee <hanlee12@gmail.com>
2016-09-23 19:46:32 +0000

    working on ride model spec

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,24 @@ class Ride < ActiveRecord::Base
   # write associations here
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    if !enough_tickets? && tall_enough?
+       "Sorry. You do not have enough tickets the #{self.attraction.name}."
+    elsif tall_enough? && !enough_tickets
+       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    elsif !enough_tickets? && !tall_enough?
+      "Sorry. You do not have enough tickets the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    end
+  end
+
+  private
+  def enough_tickets?
+    self.user.tickets >= self.attraction.tickets
+  end
+
+  def tall_enough?
+    self.user.height >= self.attraction.min_height
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
   # write associations here
   has_many :rides
-  has_many :users, :through => :rides
+  has_many :attractions, :through => :rides
+
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+
+  def mood
+    if self.nausea < self.happiness
+      "happy"
+    else
+      "sad"
+    end
+  end
+
 end
